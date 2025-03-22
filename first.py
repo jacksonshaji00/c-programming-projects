48b1b908ae08783bc7016ff152fd55e4c266d228
Jackson <jacksonshaji826@gmail.com>
2025-03-22 13:52:36 +0530

    create first.py

---
+++ first.py
@@ -0,0 +1,9 @@
+# Simple program to add two numbers
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Adding two numbers
+sum = num1 + num2
+
+# Display the sum
+print("The sum of", num1, "and", num2, "is:", sum)
